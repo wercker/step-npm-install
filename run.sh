df499656f35acace6ebaa6004c01ec54e6b0bc23
Dom Udall <dom@synthmedia.co.uk>
2013-12-02 17:20:23 +0000

    Set tmp directory as well as cache for specific module installs

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 mkdir -p $WERCKER_CACHE_DIR/wercker/npm
 npm config set cache $WERCKER_CACHE_DIR/wercker/npm
+npm config set tmp $WERCKER_CACHE_DIR/wercker/tmp
 npm install $WERCKER_NPM_INSTALL_OPTIONS
